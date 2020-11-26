86cd1809ac81e0ec4d275e29b83cf68bb9bed398
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-11-25 21:29:52 -0300

    new lessons

--- lessons/06_state-variables.js
+++ lessons/06_state-variables.js
@@ -37,4 +37,19 @@ Activity: Add a state variable to src/App.jsx
 git add src/App.jsx
 git commit -m "add a state variable for students"
 git push
+
+---------------------------------------------
+
+Explaining this.setState
+How do we change data within a state variable?
+
+Unfortunately, we can't just do this.state.studentsList = ..... to change data.
+
+To set the state of a state variable we have to use the method, this.setState.
+
+Take a look at the method on line 30 in src/App.jsx
+
+When this method is called, it is adding assignmentName to our state variable assignments by setting the state of assignments to itself with the concatenation of assignmentName.
+
+We are going to set some state in next steps.
 */
---
+++ lessons/07_create-a-method.js
@@ -0,0 +1,19 @@
+/*
+Step 7: Create a method
+Now that we see how it works with assignments, let's try it with students! We will create an addStudent method to add students the same way we add assignments.
+
+Activity: Create an addStudent method in src/App.jsx
+
+1.On line 37 of src/App.jsx, create a method called addStudent below the comment
+    addStudent should take studentName as a parameter
+    addStudent should concat studentName to the end of students list that we created earlier
+    Hint: You can copy the same format for the addAssignment method
+2.Save your changes
+3.To run your code, move inside your repo folder in your terminal and run npm start
+4.Exit the process in your terminal using Ctrl + C
+5.Stage, commit, and push your changes to the changes branch:
+
+git add src/App.jsx
+git commit -m "create addStudent method"
+git push
+*/
