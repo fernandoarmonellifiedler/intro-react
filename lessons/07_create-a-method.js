fc5cc62ca81ca528e0948d8ecb1edb2cae490797
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-11-25 21:32:53 -0300

    bind addStudent to App

--- lessons/07_create-a-method.js
+++ lessons/07_create-a-method.js
@@ -16,4 +16,21 @@ Activity: Create an addStudent method in src/App.jsx
 git add src/App.jsx
 git commit -m "create addStudent method"
 git push
+
+---------------------------------------------
+
+Step 8: Binding a method
+Since we created a method that changes a state variable, we have to bind it to our class. Then, when we call it, we know what method to reference.
+
+Activity: Bind addStudent to App
+
+1.Uncomment line 18 of src/App.jsx to bind addStudent to App
+2.Save your changes
+3.To run your code, move inside your repo folder in your terminal and run npm start
+4.Exit the process in your terminal using Ctrl + C
+5.Stage, commit, and push your changes to the changes branch:
+
+git add src/App.jsx
+git commit -m "bind addStudent to App"
+git push
 */
